dd236026761cdfaa3cdea6575a986388aed4dfaa
Surraiya Islam <i.surraiya@qa-academy.by>
2023-05-30 12:31:26 +0600

    Removed getAllAuthors() method, Used ExecuteQueryUtil to execute sql and edited AuthorDao and IAuthorDao according to it.

--- src/main/java/Database/DataAccessObjects/Implementation/AuthorDao.java
+++ src/main/java/Database/DataAccessObjects/Implementation/AuthorDao.java
@@ -1,68 +1,44 @@
 package Database.DataAccessObjects.Implementation;
 
-import Database.Connection.DatabaseConnection;
 import Database.DataAccessObjects.Interface.IAuthorDao;
+import Database.DataAccessObjects.ResultSetMapper.AuthorMapper;
 import Database.DataEntity.Entities.Author;
 import Database.DataEntity.EntityColumns.AuthorColumns;
+import Database.DbUtil.ExecuteQueryUtil;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static Database.DataAccessObjects.EntityMapper.AuthorMapper.*;
 import static Database.DataEntity.EntityColumns.AuthorColumns.*;
-import static Database.DataEntity.EntityColumns.Columns.FIRST;
 
 public class AuthorDao implements IAuthorDao {
-    private final Connection connection;
-    private Statement statement;
-    private PreparedStatement preparedStatement;
-    private ResultSet resultSet;
     private final String addAuthorQuery = String.format("INSERT INTO author(%s) VALUES (?,?,?)",
             Stream.of(AuthorColumns.values())
                     .filter(column -> column != ID)
                     .map(column -> column.columnName)
                     .collect(Collectors.joining(",")));
-    private final String getAllAuthorsQuery = "SELECT * FROM author";
     private final String getAuthorByLoginQuery = String.format("SELECT * FROM author WHERE %s = ?", LOGIN.columnName);
 
-    public AuthorDao(){
-        connection = DatabaseConnection.getDatabaseConnection();
+
+    @Override
+    public int addAuthor(Author author) {
+        Object[] parameters = {
+                author.getName(),
+                author.getLogin(),
+                author.getEmail()
+        };
+        return ExecuteQueryUtil.executeUpdate(addAuthorQuery, parameters);
     }
 
     @Override
-    public boolean addAuthor(Author author) throws SQLException {
-        preparedStatement = connection.prepareStatement(addAuthorQuery);
-        mapAuthorToInsertStatement(preparedStatement,author);
-        return preparedStatement.execute();
+    public Author getAuthorByLogin(String login) {
+        ResultSet resultSet = ExecuteQueryUtil.executeQueryWithParameter(getAuthorByLoginQuery, login);
+        return AuthorMapper.extractAuthorFromResultSet(resultSet);
     }
 
-    @Override
-    public List<Author> getAllAuthors() throws SQLException {
-        statement = connection.createStatement();
-        resultSet = statement.executeQuery(getAllAuthorsQuery);
-
-        List<Author> authors = new ArrayList<>();
-        while (resultSet.next()){
-            authors.add(extractAuthorFromResultSet(resultSet));
-        }
-
-        return authors;
-    }
-
-    @Override
-    public Author getAuthorByLogin(String login) throws SQLException {
-        preparedStatement = connection.prepareStatement(getAuthorByLoginQuery);
-        preparedStatement.setString(FIRST.index, login);
-        resultSet = preparedStatement.executeQuery();
-        return resultSet.next() ? extractAuthorFromResultSet(resultSet) : null;
-    }
-
-    public boolean ifAuthorExistsInDb(String login) throws SQLException {
-        List<Author> authors = getAllAuthors();
-        return authors.stream()
-                .anyMatch(author -> author.getLogin().equals(login));
+    public boolean ifAuthorExistsInDb(String login) {
+        Author author = getAuthorByLogin(login);
+        return author != null;
     }
 }
--- src/main/java/Database/DataAccessObjects/Interface/IAuthorDao.java
+++ src/main/java/Database/DataAccessObjects/Interface/IAuthorDao.java
@@ -2,12 +2,8 @@ package Database.DataAccessObjects.Interface;
 
 import Database.DataEntity.Entities.Author;
 
-import java.sql.SQLException;
-import java.util.List;
-
 public interface IAuthorDao {
 
-    boolean addAuthor(Author author) throws SQLException;
-    List<Author> getAllAuthors() throws SQLException;
-    Author getAuthorByLogin(String login) throws SQLException;
+    int addAuthor(Author author);
+    Author getAuthorByLogin(String login);
 }
